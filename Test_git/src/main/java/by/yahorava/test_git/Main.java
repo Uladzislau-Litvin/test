a073faa801ccc136f386e572e7668149b3018c1b
andreimozgo <andrei.mozgo@gmail.com>
2017-11-20 11:52:34 +0300

    Changed Main

--- Test_git/src/main/java/by/yahorava/test_git/Main.java
+++ Test_git/src/main/java/by/yahorava/test_git/Main.java
@@ -3,6 +3,6 @@ package by.yahorava.test_git;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world2!");
+        System.out.println("Hello world1112!");
     }
 }
